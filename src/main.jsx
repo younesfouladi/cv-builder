9304528ba02cedfa9ebe18a493cf9f97384f8c78
Younes <mehrabtahami@gmail.com>
2025-08-12 18:27:18 +0330

    Fix Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./styles/global/reset.css";
 import "./styles/global/utilities.css";
 import App from "./App.jsx";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
